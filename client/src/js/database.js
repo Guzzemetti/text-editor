8cd7293b72effe376c1c892a2fa4af9ce7723299
John <johnguzzettaiv@gmail.com>
2022-05-27 23:56:50 -0400

    Updated database.js. Now uses local storage to ensure data is stored and recalled

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, text: content });
+  const request = store.put({ text: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
@@ -32,58 +32,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
-
-
-
-
-// Activity 26
-// export const putDb = async (id, content) => {
-//   console.log('PUT to the database');
-//   const todosDb = await openDB('todos', 1);
-//   const tx = todosDb.transaction('todos', 'readwrite');
-//   const store = tx.objectStore('todos');
-//   const request = store.put({ id: id, todo: content });
-//   const result = await request;
-//   console.log('🚀 - data saved to the database', result);
-// };
-
-// Mini Project
-// Export a function we will use to GET to the database.
-// export const getDb = async () => {
-//   console.log('GET from the database');
-
-//   // Create a connection to the database database and version we want to use.
-//   const contactDb = await openDB('contact', 1);
-
-//   // Create a new transaction and specify the database and data privileges.
-//   const tx = contactDb.transaction('contact', 'readonly');
-
-//   // Open up the desired object store.
-//   const store = tx.objectStore('contact');
-
-//   // Use the .getAll() method to get all data in the database.
-//   const request = store.getAll();
-
-//   // Get confirmation of the request.
-//   const result = await request;
-//   console.log('result.value', result);
-//   return result;
-// };
-
-// or
-
-// // Activity 26
-// export const getAllDb = async () => {
-//   console.log('GET all from the database');
-//   const todosDb = await openDB('todos', 1);
-//   const tx = todosDb.transaction('todos', 'readonly');
-//   const store = tx.objectStore('todos');
-//   const request = store.getAll();
-//   const result = await request;
-//   console.log('result.value', result);
-//   return result;
-// };
